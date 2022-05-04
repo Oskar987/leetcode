dd46ef1ce67a7f6fd11e27361d0823909b101109
Pavel K <el_passa@yahoo.com>
2022-05-04 23:36:31 +0400

    two sum c#

---
+++ twosome.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        var map = new Dictionary<int, int>();
+        
+        for(int i = 0; i < nums.Length; i++){
+            var complement = target - nums[i];
+            if(map.ContainsKey(complement)){
+                return new int[]{i, map[complement]};
+            }
+            
+            map[nums[i]] = i;
+        }
+        
+        return null;
+    }
+}
